4dc9fa81e23c38894602753807cadea7f146bb2d
samwoodworth <swoodworth177@gmail.com>
2021-12-08 18:44:43 -0500

    Fixing database init

--- src/main/java/api/issues/config/LoadDatabase.java
+++ src/main/java/api/issues/config/LoadDatabase.java
@@ -2,6 +2,7 @@ package api.issues.config;
 
 import api.issues.model.Issue;
 import api.issues.repo.IssueRepo;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -9,14 +10,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class LoadDatabase {
-    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+class LoadDatabase {
 
-    @Bean
-    CommandLineRunner initDatabase(IssueRepo repo) {
-        return args -> {
-            log.info("Preloading " + repo.save(new Issue("Issue 1", "Sam Woodworth")));
-            log.info("Preloading " + repo.save(new Issue("Issue 2", "Sam Woodworth")));
-        };
-    }
+  private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+  @Bean
+  CommandLineRunner initDatabase(IssueRepo repository) {
+
+    return args -> {
+      log.info("Preloading " + repository.save(new Issue("Issue 1", "Sam Woodworth")));
+      log.info("Preloading " + repository.save(new Issue("Issue 2", "Sam Woodworth")));
+    };
+  }
 }
--- src/main/java/api/issues/model/Issue.java
+++ src/main/java/api/issues/model/Issue.java
@@ -1,5 +1,7 @@
 package api.issues.model;
 
+import java.util.Objects;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -43,5 +45,26 @@ public class Issue {
 
     public void setCreatorName(String creatorName) {
         this.creatorName = creatorName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+  
+        if (this == o)
+            return true;
+        if (!(o instanceof Issue))
+            return false;
+        Issue issue = (Issue) o;
+        return Objects.equals(this.id, issue.id) && Objects.equals(this.issue, issue.issue) && Objects.equals(this.creatorName, issue.creatorName);
+    }
+  
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.issue, this.creatorName);
+    }
+  
+    @Override
+    public String toString() {
+      return "Issue{" + "id=" + this.id + ", issue='" + this.issue + '\'' + ", creatorName='" + this.creatorName + '\'' + '}';
     }
 }
