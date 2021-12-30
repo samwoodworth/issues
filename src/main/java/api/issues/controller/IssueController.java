af38efe2e955a813b0f65f669e0c398d9df30593
samwoodworth <swoodworth177@gmail.com>
2021-12-30 14:58:00 -0500

    auth get request

--- src/main/java/api/issues/controller/IssueController.java
+++ src/main/java/api/issues/controller/IssueController.java
@@ -1,14 +1,76 @@
 package api.issues.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import api.issues.exceptions.IssueNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import api.issues.repo.IssueRepo;
+import api.issues.model.Issue;
+
+/*
+String authenticated() {
+    if is authenticated
+        let through to api
+    else forward to login
+}
+ */
+
+@RestController
 public class IssueController {
 
-    boolean loggedIn = true;
+    private final IssueRepo repo;
 
-    //@RequestMapping("/")
+    //Repo constructor
+    IssueController(IssueRepo repo) {
+        this.repo = repo;
+    }
 
+    boolean isAuthed() {
+        boolean authorized = true;
+        if (authorized)
+            return true;
+        else
+            return false;
+    }
 
+    //http://localhost:8081/get_issues
+    @GetMapping("/get_issues")
+    ResponseEntity<?> all() {
+        if (isAuthed()) {
+            return new ResponseEntity<>(repo.findAll(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<String>("Unauthorized", HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    //http://localhost:8081/get_issue?id=1
+    @GetMapping("/get_issue")
+    ResponseEntity<?> one(@RequestParam Long id) {
+        //If not found return badRequest http status
+        if (isAuthed()) {
+            Issue foundIssue =  repo.findById(id)
+                    .orElseThrow(() -> new IssueNotFoundException(id));
+            return new ResponseEntity<>(foundIssue, HttpStatus.OK);
+        } else {
+
+            return new ResponseEntity<String>("Unauthorized", HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    //Return issue or void?
+    //http://localhost:8081/insert_issue + Issue JSON
+    @PostMapping("/insert_issue")
+    ResponseEntity<?> insertIssue(@RequestBody Issue newIssue) {
+        if (isAuthed()) {
+            return new ResponseEntity<>(repo.save(newIssue), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<String>("Unauthorized", HttpStatus.UNAUTHORIZED);
+        }    
+    }
 }
+
+/*
+    RestController in security to get loggedin status
+    If true then allow through, if false redirect to login service
+*/
--- src/main/java/api/issues/interceptor/AuthInterceptor.java
+++ src/main/java/api/issues/interceptor/AuthInterceptor.java
@@ -2,7 +2,11 @@ package api.issues.interceptor;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,16 +16,26 @@ public class AuthInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("Prehandle method");
+
+        URL url = new URL("http://localhost:8080/getAuth");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println("Content: " + content);
+            content.append(inputLine);
+        }
+        in.close();
+
+        con.disconnect();
+
+        System.out.println("Input line: " + inputLine);
+
         return true;
     }
 
-/*    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
-    }
 
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
-    }*/
 }
