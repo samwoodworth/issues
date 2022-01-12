d6b0778dad7762fc902d9536f5388c116c5e1a11
samwoodworth <swoodworth177@gmail.com>
2022-01-12 10:02:25 -0500

    finished

--- src/main/java/api/issues/controller/IssueController.java
+++ src/main/java/api/issues/controller/IssueController.java
@@ -1,6 +1,7 @@
 package api.issues.controller;
 
 import api.issues.exceptions.IssueNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import api.issues.model.Issue;
 @RestController
 public class IssueController {
 
+    @Autowired
     private final IssueRepo repo;
 
     //Repo constructor
@@ -18,46 +20,25 @@ public class IssueController {
         this.repo = repo;
     }
 
-    boolean isAuthed() {
-        boolean authorized = true;
-        if (authorized)
-            return true;
-        else
-            return false;
-    }
-
     //http://localhost:8081/get_issues?user=admin
     @GetMapping("/get_issues")
     ResponseEntity<?> all(@RequestParam String user) {
-        if (isAuthed()) {
-            return new ResponseEntity<>(repo.findAll(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<String>("Unauthorized", HttpStatus.UNAUTHORIZED);
-        }
+        return new ResponseEntity<>(repo.findAll(), HttpStatus.OK);
     }
 
     //http://localhost:8081/get_issue?id=1&user=admin
     @GetMapping("/get_issue")
     ResponseEntity<?> one(@RequestParam("id") Long id, @RequestParam("user") String user) {
         //If not found return badRequest http status
-        if (isAuthed()) {
-            Issue foundIssue =  repo.findById(id)
+        Issue foundIssue =  repo.findById(id)
                 .orElseThrow(() -> new IssueNotFoundException(id));
-            return new ResponseEntity<>(foundIssue, HttpStatus.OK);
-        } else {
-            //Not used
-            return new ResponseEntity<String>("Unauthorized", HttpStatus.UNAUTHORIZED);
-        }
+        return new ResponseEntity<>(foundIssue, HttpStatus.OK);
     }
 
     //Return issue or void?
     //http://localhost:8081/insert_issue?user=admin + Issue JSON
     @PostMapping("/insert_issue")
     ResponseEntity<?> insertIssue(@RequestBody Issue newIssue, @RequestParam String user) {
-        if (isAuthed()) {
-            return new ResponseEntity<>(repo.save(newIssue), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<String>("Unauthorized", HttpStatus.UNAUTHORIZED);
-        }    
+        return new ResponseEntity<>(repo.save(newIssue), HttpStatus.OK);
     }
 }
