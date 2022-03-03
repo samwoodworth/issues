ec2cb1790d0150bcaa45aafde0a726b57cbe93f3
samwoodworth <swoodworth177@gmail.com>
2022-03-02 23:37:37 -0500

    Interceptor stuff

--- src/main/java/api/issues/interceptor/AuthInterceptor.java
+++ src/main/java/api/issues/interceptor/AuthInterceptor.java
@@ -17,6 +17,7 @@ public class AuthInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
+        System.out.println("Interceptor");
         String responseBody;
         String username = request.getParameter("user");
 
--- src/main/java/api/issues/interceptor/AuthInterceptorConfig.java
+++ src/main/java/api/issues/interceptor/AuthInterceptorConfig.java
@@ -13,6 +13,6 @@ public class AuthInterceptorConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authInterceptor);
+        registry.addInterceptor(authInterceptor).excludePathPatterns("/");
     }
 }
