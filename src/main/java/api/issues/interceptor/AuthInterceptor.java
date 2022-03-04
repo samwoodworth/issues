204d042d51130fbcfa9dd885bc6b6662df5dc488
samwoodworth <swoodworth177@gmail.com>
2022-03-04 14:33:14 -0500

    JSESSION cookie check

--- src/main/java/api/issues/interceptor/AuthInterceptor.java
+++ src/main/java/api/issues/interceptor/AuthInterceptor.java
@@ -16,45 +16,15 @@ public class AuthInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-//        System.out.println("Interceptor");
-        String responseBody;
-        String username = request.getParameter("user");
-
         URLConnection con = new URL("http://localhost:8080/getAuth").openConnection();
         InputStream inputStream = con.getInputStream();
-
-        try (Scanner scanner = new Scanner(inputStream)) {
-            responseBody = scanner.useDelimiter("\\A").next();
-        }
 
         //Checking for JSESSIONID cookie
         Cookie[] cookies = request.getCookies();
         for (Cookie cookie : cookies) {
-            System.out.println("Cookies are: " + cookie.getName());
+            if (cookie.getName().equals("JSESSIONID"))
+                return true;
         }
-
-        CookieManager cookieManager = new CookieManager();
-        CookieHandler.setDefault(cookieManager);
-
-
-        //If JESSIONID exists then true, if not then false
-        //Check if logout works with it
-        String headerName;
-        for (int i = 1; (headerName = con.getHeaderFieldKey(i)) != null; i++) {
-            System.out.println("Header name is: " + headerName);
-            if (headerName.equals("Set-Cookie")) {
-                String cookie = con.getHeaderField(i);
-//                System.out.println("Cookie is: " + cookie);
-                String cookieName = cookie.substring(0, cookie.indexOf("="));
-                String cookieValue = cookie.substring(cookie.indexOf("=") + 1);
-//                System.out.println("Name is: " + cookieName + " and value is: " + cookieValue);
-
-                if (cookieName.equals("username") && !cookieValue.equals(null)) {
-                    return true;
-                }
-            }
-        }
-
         return false;
     }
 }
