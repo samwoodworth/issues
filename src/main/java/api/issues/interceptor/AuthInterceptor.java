b7c9b005df3e4dc3d6b484b5fce89391648bebd8
samwoodworth <swoodworth177@gmail.com>
2022-03-04 00:20:19 -0500

    cookie authentication

--- src/main/java/api/issues/interceptor/AuthInterceptor.java
+++ src/main/java/api/issues/interceptor/AuthInterceptor.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import java.io.InputStream;
 import java.net.*;
 import java.util.Scanner;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -15,7 +16,7 @@ public class AuthInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-        System.out.println("Interceptor");
+//        System.out.println("Interceptor");
         String responseBody;
         String username = request.getParameter("user");
 
@@ -26,57 +27,34 @@ public class AuthInterceptor implements HandlerInterceptor {
             responseBody = scanner.useDelimiter("\\A").next();
         }
 
+        //Checking for JSESSIONID cookie
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            System.out.println("Cookies are: " + cookie.getName());
+        }
+
         CookieManager cookieManager = new CookieManager();
         CookieHandler.setDefault(cookieManager);
 
+
+        //If JESSIONID exists then true, if not then false
+        //Check if logout works with it
         String headerName;
         for (int i = 1; (headerName = con.getHeaderFieldKey(i)) != null; i++) {
+            System.out.println("Header name is: " + headerName);
             if (headerName.equals("Set-Cookie")) {
                 String cookie = con.getHeaderField(i);
-                System.out.println("Cookie is: " + cookie);
+//                System.out.println("Cookie is: " + cookie);
                 String cookieName = cookie.substring(0, cookie.indexOf("="));
                 String cookieValue = cookie.substring(cookie.indexOf("=") + 1);
-                System.out.println("Name is: " + cookieName + " and value is: " + cookieValue);
+//                System.out.println("Name is: " + cookieName + " and value is: " + cookieValue);
 
+                if (cookieName.equals("username") && !cookieValue.equals(null)) {
+                    return true;
+                }
             }
         }
 
-/*        if (username != null){
-            URLConnection con = new URL("http://localhost:8080/getAuth" + username).openConnection();
-            InputStream inputStream = con.getInputStream();
-
-            try (Scanner scanner = new Scanner(inputStream)) {
-                responseBody = scanner.useDelimiter("\\A").next();
-            }
-
-            CookieManager cookieManager = new CookieManager();
-            CookieHandler.setDefault(cookieManager);
-
-            String headerName;
-            for (int i=1; (headerName = con.getHeaderFieldKey(i))!=null; i++) {
-                if (headerName.equals("Set-Cookie")) {
-                    String cookie = con.getHeaderField(i);
-                    System.out.println("Cookie is: " + cookie);
-//                    cookie = cookie.substring(0, cookie.indexOf(";"));
-                    String cookieName = cookie.substring(0, cookie.indexOf("="));
-                    String cookieValue = cookie.substring(cookie.indexOf("=") + 1);
-                    System.out.println("Name is: " +cookieName + " and value is: " + cookieValue);
-
-                }
-            }*/
-
-/*            if (responseBody.equals("true")) {
-                return true;
-            } else {
-                //System.out.println("Not signed in.\n");
-                response.setStatus(401);
-
-                //response.sendRedirect("http://localhost:8080/login");  //Doesn't give 401 status
-                return false;
-            }
-        } else
-            //System.out.println("No parameter passed.\n");
-            return false;*/
-        return true;
+        return false;
     }
 }
